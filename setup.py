d8fda8afb6388810e83fd9ea6bc89e6ee1e9c914
Thomas Levine <_@thomaslevine.com>
2016-03-11 13:18:22 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(name='cutaudio',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='Cut an audio file into small pieces with nice names.',
+      url='http://dada.pink/cutaudio',
+      scripts=['cutaudio'],
+      install_requires = ['horetu'],
+      version='0.0.1',
+      license='AGPL',
+      classifiers=(
+          'Programming Language :: Python :: 3.5',
+      ),
+)
